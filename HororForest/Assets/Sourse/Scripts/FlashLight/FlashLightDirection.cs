3a80964fd9a30639e17df4895f8e32c13760dc07
SelfSr <alexey_belochkin@mail.ru>
2023-09-15 22:15:20 +0300

    Add third Location

--- HororForest/Assets/Sourse/Scripts/FlashLight/FlashLightDirection.cs
+++ HororForest/Assets/Sourse/Scripts/FlashLight/FlashLightDirection.cs
@@ -8,7 +8,7 @@ public class FlashLightDirection : MonoBehaviour
 
     void Update()
     {
-        Ray desiredTargetRay = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector2(Screen.width / 2, -Screen.height + 300));
+        Ray desiredTargetRay = mainCamera.GetComponent<Camera>().ScreenPointToRay(new Vector2(Screen.width / 2, -Screen.height + 400));
         Vector3 desiredTargetPosition = desiredTargetRay.origin + desiredTargetRay.direction * targetRange;
         aimTarget.position = desiredTargetPosition;
     }
